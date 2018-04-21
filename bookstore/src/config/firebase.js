86a82ce89f411ef2b2cc72a6788f5b168f60fd90
josue.rocafuertef@ug.edu.ec <josue.rocafuertef@ug.edu.ec>
2018-04-21 16:24:45 -0500

    WEBPACK

---
+++ bookstore/src/config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+
+    apiKey: "AIzaSyC94JlhhHJjTr9CYoVyxpEI_LiqxMt-vyA",
+    authDomain: "bookstore-7b3ec.firebaseapp.com",
+    databaseURL: "https://bookstore-7b3ec.firebaseio.com",
+    projectId: "bookstore-7b3ec",
+    storageBucket: "bookstore-7b3ec.appspot.com",
+    messagingSenderId: "107954555633"
+
+};
